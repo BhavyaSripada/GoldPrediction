cd94d8d2bdd1f56896120a5624383146c79b72dc
BhavyaSripada <109012980+BhavyaSripada@users.noreply.github.com>
2023-09-18 09:58:42 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,8 +1,7 @@
 
 import streamlit as st
 import pandas as pd
-#import sklearn
-sudo pip3 install scikit-learn
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR 
 import numpy as np
